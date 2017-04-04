0baed6e857e784cfb8fd3bc5738ad94079c7aefa
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-04-05 08:55:48 +1000

    Add a simple list

--- Styles.js
+++ Styles.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from 'react-native';
+import Expo from 'expo';
 
 export default styles = StyleSheet.create({
   container: {
+    marginTop: Expo.Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- components/ChuckNorrisScreen.js
+++ components/ChuckNorrisScreen.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ListView } from 'react-native';
 import styles from '../Styles.js';
 
 class ChuckNorrisScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>List of Chuck Norris jokes</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
       </View>
     )
   }
