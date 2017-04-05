de1464db3419f6c5e012bd818ffa81f90500c23f
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-04-06 08:26:12 +1000

    Change the main text styles

--- Styles.js
+++ Styles.js
@@ -4,9 +4,17 @@ import Expo from 'expo';
 export default styles = StyleSheet.create({
   container: {
     marginTop: Expo.Constants.statusBarHeight,
+    marginLeft: 20,
+    marginRight: 20,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  mainText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'cadetblue'
+  }
 });
