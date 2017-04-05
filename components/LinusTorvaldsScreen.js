180605df32e118caa4e8a34e18c6e102a0656dfa
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-04-06 08:11:11 +1000

    Add the ICNDB fetch

--- components/LinusTorvaldsScreen.js
+++ components/LinusTorvaldsScreen.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import styles from '../Styles.js';
 
 class LinusTorvaldsScreen extends React.Component {
+  fetchData() {
+    const resource = 'http://api.icndb.com/jokes/random?' +
+      'firstName=Linus&' +
+      'lastName=Torvalds&' +
+      'escape=javascript&' +
+      'limitTo=[nerdy]&' +
+      'exclude=[explicit]'
+    return fetch(resource)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          joke: responseData.value.joke
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .done();
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      joke: null
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    if (!this.state.joke) {
+      return this.renderLoadingView();
+    } else {
+      return this.renderJokeView();
+    }
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text>List of Linus Torvalds jokes</Text>
+        <ActivityIndicator size="large"/>
+      </View>
+    )
+  }
+
+  renderJokeView() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.joke}</Text>
       </View>
     )
   }
